652b374771b5e3ef46c436880fe7330f90c375dc
LittleSeneca <43323252+LittleSeneca@users.noreply.github.com>
2021-02-01 08:47:47 -0800

    Changed YouCompleteMe git formatting

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ youcompleteme_configure () {
 	cd ${nvim_dir}/bundle/
 	apt install build-essential cmake vim-nox python3-dev -y
 	apt install mono-complete golang nodejs default-jdk npm -y
-	git clone --recurse-submodules git@github.com:ycm-core/YouCompleteMe.git 
+	git clone --recurse-submodules https://github.com/ycm-core/YouCompleteMe.git
 	python3 install.py --clangd-completer
 	echo "Finished Installing You Complete Me"
 }
