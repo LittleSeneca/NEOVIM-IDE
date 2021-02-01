f668fef1a0999965cc12fa5dec36e177293f9377
LittleSeneca <graham.brooks@protonmail.com>
2021-01-31 20:55:45 -0800

    debugging

--- install.sh
+++ install.sh
@@ -5,20 +5,24 @@ IFS=$'\n\t'
 # First, we need to download and then configure neovim.
 # I am going to install neovim to the /usr/local/bin directory
 variables () {
+	echo "Setting Variables"
 	export root_dir=$(pwd)
 	export config_dir=${root_dir}/config/
 	export bin_dir="/usr/local/bin"
 	export nvim_dir="${HOME}/.config/nvim/"
 	export nvim_url="https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz"
 	export vimplug_url="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+	echo "Finished setting variables"
 }
 neovim_install () {
+	echo "Installing NEOVIM"
 	cd ${bin_dir}
 	wget ${nvim_url}
 	tar zxvf nvim-linux64.tar.gz
 	echo "export PATH=${nvim_dir}:$PATH" >> ${HOME}/.bashrc
 	echo "alias vim='nvim'" >> ${HOME}/.bashrc
 	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs ${vimplug_url}'
+	echo "Finished Installing NOVIM"
 }
 neovim_configure () {
 	mkdir ${nvim_dir} -p
@@ -26,11 +30,13 @@ neovim_configure () {
 	nvim --headless +PlugInstall +qall
 }
 youcompleteme_configure () {
+	echo "Installing You Complete Me Libraries"
 	mkdir ${nvim_dir}/bundle -p
 	cd ${nvim_dir}/bundle/
 	git clone https://github.com/ycm-core/YouCompleteMe.git
 	git submodule update --init --recursive
 	python3 install.py --clangd-completer
+	echo "Finished Installing You Complete Me"
 }
 variables
 neovim_install
